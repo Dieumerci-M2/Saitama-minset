37f12561465c0a1e2bdcd2ab593570255760b3b2
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-01-31 13:27:22 +0100

    feat: initial commit

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../components/Login'
+import Register from '../components/Register'
+
+function Auth() {
+  return (
+    <div>
+        <Login />
+        <Register />
+
+    </div>
+  )
+}
+
+export default Auth
