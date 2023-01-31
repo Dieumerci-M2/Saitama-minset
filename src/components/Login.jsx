209a0026ae1084e0c828ba733aa335dd066ede50
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-01-31 16:00:43 +0100

    feat: add login tailwind component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,81 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-1xl font-normal tracking-tight text-gray-900">
+              VOUS AVEZ DÉJÀ UN COMPTE ? <span className='ml-2 text-[red] cursor-pointer text:hover-indigo-600'> S'inscrire</span>
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Votre adresse mail
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Mot de passe
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  Se souvenir de moi
+                </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                Mot de passe oublié ?
+                </a>
+              </div>
+            </div>
+
+            <div>
+              <button 
+                type="submit"
+                className="flex bg-[red] group relative flex w-full justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="flex absolute inset-y-0 left-0 flex items-center pl-3">
+                </span>
+                S'inscrire
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
