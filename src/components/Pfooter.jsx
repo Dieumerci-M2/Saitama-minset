96ae7326590c5c95ea4d6b600bb570f8ec0ab5b4
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-02-03 06:05:13 +0100

    feat: finish accueil style

--- src/components/Pfooter.jsx
+++ src/components/Pfooter.jsx
@@ -7,16 +7,24 @@ import telegram from "../assets/logos_telegram.png"
 
 const Pfooter = () => {
   return (
-      <footer>
-          <div className='w-[150px] h-[50px]'><img src={ saitama } alt="Logo"/></div>
-          <div>
-              <span>
-                  <img src={ facebook } alt="Logo" />
-                  <img src={ telegram } alt="Logo" />
-                  <img src={ twiter } alt="Logo" />
-                 <img src={ linkedin } alt="Logo"/> 
+      <footer className='overflow-hidden bg-gray-200
+       flex flex-col justify-between align-center text-center'
+      >
+          <div className='flex justify-center align-center text-center'>
+              <div className='m-5'><img src={ saitama } alt="Logo" className="w-[80px] h-[30px]" /></div>
+          </div>
+          <div className='flex flex-col text-center'>
+              <div className='flex justify-center align-center text-center gap-2'>
+                  <img className='w-[30px] h-[30px] cursor-pointer' src={ facebook } alt="Logo" />
+                  <img className='w-[30px] h-[30px] cursor-pointer' src={ telegram } alt="Logo" />
+                  <img className='w-[30px] h-[30px] cursor-pointer' src={ twiter } alt="Logo" />
+                 <img className='w-[30px] h-[30px] cursor-pointer' src={ linkedin } alt="Logo"/> 
+              </div>
+              <span
+                  className='mt-10'
+              >
+                  All rights reserved . © 2023 SAITAMA-MINDSET. Powored by Colab-Group
               </span>
-              <span>All rights reserved . © 2023 SAITAMA-MINDSET. Powored by Colab-Group</span>
           </div>
       </footer>
   )
