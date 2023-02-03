06ab3bf4ceb9eab2b9bdefb4c606c237c852ed9b
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-02-03 10:53:38 +0100

    feat: refactor ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,15 @@ import saitama from "../components/img/saitama.png"
 function Navbar() {
   return (
     <div>
-
-<header className='flex flex-row justify-between bg-white border-b-2 border-[gray]-600'>
-              <div className='border w-[100px] h-[20px] m-2'><img src={saitama} alt="Logo"/></div>
-              <div className='flex gap-10
+      <header className='flex flex-row justify-between bg-white border-b-2 border-[gray]-600'>
+        <div className='border w-[100px] h-[20px] m-10'><img src={saitama} alt="Logo" /></div>
+        <div className='flex gap-10
                m-3 cursor-pointer mr-10 font-medium text-lg'
-              >
-                  <span className='text-[red] text-1xl ' id='inscription'>Inscription</span>
-                  <span className='text-[red] text-1xl' id='connexion'>Connexion</span>
-              </div>
-          </header>
+        >
+          <span className='text-[red] text-1xl ' id='inscription'>Inscription</span>
+          <span className='text-[red] text-1xl' id='connexion'>Connexion </span>
+        </div>
+      </header>
 
     </div>
   )
