76f87cb6e460a5bc01045f53e9e63f7d44f430a8
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-02-04 20:15:07 +0100

    feat: add categories search component

--- src/components/CategoriesSearch.jsx
+++ src/components/CategoriesSearch.jsx
@@ -6,17 +6,17 @@ function CategoriesSearch() {
   return (
     <div className='flex flex-col p-2 w-64 border-b-2 border-[white]-600 bg-gray-200'>
       <div className='flex flex-row w-[100%] justify-between'>
-      <div className=' flex border-b-2 p-2 border-blue-400 bg-[red]'>
+      <div className=' flex border-b-2 p-2 border-blue-400 bg-red-600'>
         <span className='flex  text-white flex-row justify-around gap-2'><BsList size={40} color={"white"} />Rechercher une catégorie</span>
         </div>
       </div>
         <div className='flex flex-column flex-wrap justify-between'>  
-        <span className='border-b-2 border-blue-400 p-3 w-full'>Selon le prix des livres </span>
-        <p className='border-b-2 border-blue-400 p-3 w-full'>Selon le prix des livres</p>
-        <p className='border-b-2 border-blue-400 p-3 w-full'>Selon le prix des livres</p>
-        <p className='border-b-2 border-blue-400 p-3 w-full'>Selon le prix des livres</p>
-        <p className='border-b-2 border-blue-400 p-3 w-full' >Selon le prix des livres</p>
-        <p className='border-b-2 border-blue-400 p-3 w-full'>Selon le prix des livres</p>
+        <p className='border-b-2 border-blue-400 p-3 hover:bg-slate-400 w-full cursor-pointer duration-300'>Selon le prix des livres </p>
+        <p className='border-b-2 border-blue-400 p-3 hover:bg-slate-400 w-full cursor-pointer duration-300'>Selon le prix des livres</p>
+        <p className='border-b-2 border-blue-400 p-3 cursor-pointer hover:bg-slate-400 duration-300 w-full'>Selon le prix des livres</p>
+        <p className='border-b-2 border-blue-400 cursor-pointer hover:bg-slate-400 duration-300 p-3 w-full'>Selon le prix des livres</p>
+        <p className='border-b-2 border-blue-400 p-3 w-full hover:bg-slate-200 cursor-pointer duration-300' >Selon le prix des livres</p>
+        <p className='border-b-2 border-blue-400 p-3 w-full cursor-pointer hover:bg-slate-200 duration-300'>Selon le prix des livres</p>
     </div>
         
     </div>
