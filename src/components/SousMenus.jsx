5689842bef4aede43098cf72963b6de407cf95cd
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-02-05 18:14:31 +0100

    feat : add Sous-Menus component

--- src/components/SousMenus.jsx
+++ src/components/SousMenus.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 const SousMenus = () => {
   return (
-    <div>
-      
+    <div className="bg-gray-300 text-white border-gray-900 border-2 w-[500px] h-[40px] ">
+      <span className="p-4 cursor-pointer">Home</span>
+      <span className="p-4 cursor-pointer">Formations</span>
+      <span className="p-4 cursor-pointer">Nos livres</span>
+      <span className="p-4 cursor-pointer">Blogs</span>
+      <span className="p-4 cursor-pointer">Contact</span>
     </div>
-  )
+  );
 }
 
 export default SousMenus
