8b5b54a1a16ef4061dbe82dcf682845d07e9b804
jethronbwira@gmail.com <jethronbwira@gmail.com>
2023-02-06 14:17:28 +0100

    feat: add login form input

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import imgLog from "../components/img/imgLog.png"
+import imgLog from "../components/img/imgLog.png";
+import { Input } from '@material-tailwind/react';
 import { BsFillTelephoneOutboundFill }  from 'react-icons/bs';
 import { BsFillEnvelopeOpenFill }  from 'react-icons/bs';
 
 function ContactForm() {
   return (
     
-    <div className="flex flex-row flex-wrap items-center mt-14 ">     
+    <div className="flex flex-row items-center justify-between mt-14 ">     
         <div className="imgBackLogin">
         <p className="flex text-5xl font-bold p-10 text-white"> Entrez vos informations</p>
-        <p className="p-10 text-white mb-10">Dites quelques chose pour demarrer un chat en direct <br /><br /></p> <p><span className='flex flex-row gap-5'><BsFillTelephoneOutboundFill color ='white' /><span className=''>+2479876543218</span></span><br /><span>JJ<span></span></span><span></span></p>
+        <p className="p-5 text-white mb-10">Dites quelques chose pour demarrer un chat en direct <br /><br /><p> <span> ☎ <span>+229970580581</span> </span></p> <br /> <p className='flex gap-3'><span><BsFillEnvelopeOpenFill /></span><span>demomail@gmail.com</span></p> <br /></p>
+        </div>
+
+    <div className='flex flex-row gap-20 ml-10 w-300'>
+        <span>
+        <Input variant="standard" label="Entrez votre nom" />
+        <Input variant="standard" label="Entrez votre postnom" />
+        </span>
+<br /> <br />
+        <span>
+        <Input variant="static" label="Standard" />
+        <Input variant="static" label="Standard" />
+        </span>
+
         </div>
     </div>
   )
