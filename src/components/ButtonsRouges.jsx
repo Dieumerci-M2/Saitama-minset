860a417047047d33df50976b50b810883a94a897
Dieumerci Muhindo <dieumerci.md09@gmail.com>
2023-02-06 16:19:20 +0100

    feat : finish to stylise BoutonRouges component

--- src/components/ButtonsRouges.jsx
+++ src/components/ButtonsRouges.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 const ButtonsRouges = () => {
   return (
-    <div>ButtonsRouges</div>
-  )
+    <div className="flex flex-row justify-around my-10">
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+      <button className="bg-red-700 p-3 w-36 text-lg font-semibold text-[#FFFFFF] rounded-md ">
+        TECH
+      </button>
+    </div>
+  );
 }
 
 export default ButtonsRouges
