b04ef07a594d36ed93bea1494224a9b11082c1dd
Annette Bwemere <bwemereannette@gmail.com>
2023-02-18 14:50:34 +0200

    style:responsiveNessdeskMobileFinal

--- src/pages/Presentation.jsx
+++ src/pages/Presentation.jsx
@@ -71,93 +71,7 @@ const Presentation = () => {
           </div>
         </div>
 
-        {/* <header className="fixed  w-full">
-        <img src={saitama} alt="Logo" />
-        <section className="flex  bg-white flex-row justify-between">
-          <div className="border w-[150px]  h-[50px] m-2 ">
-          </div>
-          <div
-            className="flex gap-10
-                m-3 mr-10 font-medium text-lg">
-            <span
-              className="cursor-pointer hover:text-blue-400"
-              id="inscription"
-              onClick={() => {
-                router('/register');
-              }}>
-              Inscription
-            </span>
-            <span
-              onClick={() => {
-                router('/auth');
-              }}
-              className="cursor-pointer hover:text-blue-400"
-              id="connexion">
-              Connexion
-            </span>
-          </div>
-        </section>
-      </header> */}
-        {/* <main className="flex-col">
-        <section className="bg-gray-200 flex">
-          <div className="text-center flex-col m-10 pt-4 bg-gray-200 text-gray-200">
-            <img
-              src={Accueil}
-              alt="Accueil"
-              className="mt-8 bg-gray-200 text-gray-200"
-            />
-          </div>
-          <div className="text-center m-10 w-[50%]">
-            <p className="font-200 text-2xl pt-10">
-              Le meilleur investissement que tu puisse faire c'est d'investir dans toi même
-            </p>
-            <div className="mt-2 text-left text-lg">
-              <Button
-                onClick={() => {
-                  router('/home');
-                }}
-                className="text-lg bg-red-700 ">
-                Continuer
-              </Button>
-            </div>
-          </div>
-        </section>
-        <section className="flex flex-col">
-          <div className="flex justify-around p-10">
-            <span>
-              <img
-                src={ReadBook1}
-                alt="ReadBook"
-                className="w-[250px] h-[250px] bg-#28a69b"
-              />
-            </span>
-            <span>
-              <img
-                src={ReadBook2}
-                alt="ReadBook"
-                className="w-[250px] h-[250px]"
-              />
-            </span>
-          </div>
-          <div
-            className="flex flex-col justify-center
-               align-center text-center gap-8">
-            <span className="font-bold text-2xl">Plus tu apprends, Plus t gagnes</span>
-            <span className="">
-              <Button
-                onClick={() => {
-                  router('/home');
-                }}
-                className="text-lg bg-red-700">
-                Découvrez nos programmes
-              </Button>
-            </span>
-          </div>
-        </section>
-      </main> */}
-        {/* <footer>
-        <Pfooter />
-      </footer> */}
+       
       </nav>
 
       <section id="section">
@@ -182,28 +96,34 @@ const Presentation = () => {
         </div>
       </section>
 
-      <section id="book">
-        <div className=" container flex flex-col items-center px-6 mx-0 mt-0 space-y-0 tablette:space-y-0  tablette:flex-row">
-        <span>
-              <img
-                src={ReadBook1}
-                alt="ReadBook"
-                className="w-[250px] h-[250px] bg-#28a69b"
-              />
-            </span>
-        
-          <div className="flex flex-col mb-5 -space-y-6 tablette:w-1/2">
-          <span>
-              <img
-                src={ReadBook2}
-                alt="ReadBook"
-                className="w-[250px] h-[250px]"
-              />
+      <section className="container flex flex-col justify-center tablette:flex-col">
+        {/* <div className=" tablette:justify-around p-10"> */}
+        <div className='tablette:flex tablette:flex-row tablette:justify-around'>
+
+          <img src={ReadBook1} alt="ReadBook" className="w-[250px] h-[250px] bg-#28a69b" />
+
+          <img src={ReadBook2} alt="ReadBook" className="w-[250px] h-[250px]" />
+        </div>
+
+          <div
+            className="flex flex-col justify-center
+               align-center text-center gap-8">
+            <span className="font-bold text-2xl">Plus tu apprends, Plus t gagnes</span>
+            <span className="">
+              <Button
+                onClick={() => {
+                  router('/home');
+                }}
+                className="text-lg bg-red-700">
+                Découvrez nos programmes
+              </Button>
             </span>
           </div>
-        </div>
+        {/* </div> */}
       </section>
-      
+      <footer>
+        <Pfooter />
+      </footer>
     </>
   );
 };
