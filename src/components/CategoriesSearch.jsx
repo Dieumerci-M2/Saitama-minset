b865fcb69b77b2701804b6b212cb265e84f378f5
Jospin Kanane <jospinkanane@yahoo.com>
2023-02-19 17:45:57 +0200

    Cutting video icon, notification, hamb-menu and search-by-cat

--- src/components/CategoriesSearch.jsx
+++ src/components/CategoriesSearch.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import { BsList } from 'react-icons/bs';
-import { BsChevronRight } from 'react-icons/bs';
 
 function CategoriesSearch() {
   return (
@@ -8,9 +6,8 @@ function CategoriesSearch() {
       className="flex flex-col  p-2 w-[100%] border-b-2 border-[white]-600
      bg-slate-200 ">
       <div className="flex flex-row w-[100%] justify-between">
-        <div className=" flex border-b-2 p-2 border-blue-400 bg-red-700">
-          <span className="flex  text-white flex-row justify-around gap-2">
-            <BsList size={40} color={'white'} />
+        <div className=" flex border-b-2 p-2 border-blue-400 bg-red-700 w-full">
+          <span className="text-white text-[16px] font-bold">
             Rechercher une catégorie
           </span>
         </div>
--- src/components/Menus.jsx
+++ src/components/Menus.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Saitama from '../assets/saitama.png';
 import Search from '../assets/Vector.svg';
-import Not from '../assets/Vector(1).svg';
-import Youtube from '../assets/Vector(2).svg';
 import { useNavigate } from 'react-router-dom';
 const Menus = () => {
   const router = useNavigate();
   return (
-    <section className="flex justify-between mx-[50px]">
+    <section className="flex w-full justify-around">
       <div
         className="w-[150px]  h-[50px] my-3
        xl:mr-10">
@@ -27,20 +25,11 @@ const Menus = () => {
           placeholder="Rechercher un Livre..."
           placeholder:italic
         />
-        <select
-          className="p-2 outline-none focus:border-sky-500 focus:ring-sky-500 max-[3500px]:w-36
-        max-[1550px]:48">
-          <option>Catégories</option>
-          <option>Développement personnel </option>
-          <option>Séduction </option>
-          <option>Autres... </option>
-        </select>
         <span className="w-20 h-10 bg-red-700 cursor-pointer">
           <img src={Search} alt="Search" className="m-auto my-2 text-[#FFFFFF]" />
         </span>
       </div>
-      <div className="flex flex-row h-12 w-48 bg-gray-100 my-auto mt-3 mx-8 lg:mx-8 xl-8
-      ml-8 lg:ml-8 xl:ml-16"
+      <div className="flex flex-row h-12 w-48 bg-gray-100 my-auto mt-3 mx-8 lg:mx-8 xl-8 lg:ml-8 xl:ml-16"
       >
         <img
           className="inline-block h-10 w-10 rounded-full ring-2 ring-white m-auto mt-1 ml-1 xl:ml-4 lg:ml-1"
@@ -57,17 +46,6 @@ const Menus = () => {
           </span>
           <br />
           <span className="mr-2 cursor-pointer">Compte</span>
-        </div>
-      </div>
-      <div className="flex flex-row gap-8 mt-3 ml-6 max-[200px]:-mr-4">
-        <div className="flex flex-row gap-2 mt-1">
-          <span className="max-[3500px]:w-10">
-            <img src={Not} alt="not" className="cursor-pointer" />
-          </span>
-          <span>Notifications</span>
-        </div>
-        <div className="mt-1 xl:mr-0 max-[3500px]:w-10">
-          <img src={Youtube} alt="youtube" className="cursor-pointer" />
         </div>
       </div>
     </section>
